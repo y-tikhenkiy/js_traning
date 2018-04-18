092baada131c271e7c880354f74dae2adcc8a03a
y-tikhenkiy <y.tikhenkiy@gmail.com>
2018-04-18 13:26:03 +0300

    variables exercises

---
+++ variable.js
@@ -0,0 +1,12 @@
+var message;
+message = 'Hello!';
+
+// var message = 'Hello!'; //короткий вариант
+
+alert(message);
+
+var COLOR_RED = "#F00";
+var COLOR_GREEN = "#0F0";
+
+var color = COLOR_RED;
+alert( color );

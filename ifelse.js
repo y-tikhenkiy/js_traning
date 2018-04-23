9159150ab261f2936334594eb71ec169a0f6299f
y-tikhenkiy <y.tikhenkiy@gmail.com>
2018-04-23 15:40:07 +0300

    add new files: ifelse.js, uibasic.js edit first.html

---
+++ ifelse.js
@@ -0,0 +1,3 @@
+var year = prompt('What is the appearance year of specification ECMA-262 5.1?', '');
+
+if (year != 2011) alert('Your answer is incorrect!');
---
+++ uibasic.js
@@ -0,0 +1,10 @@
+// var years = prompt('Сколько вам лет?', 100);
+//
+// alert('Вам ' + years + 'лет!')
+
+// var isAdmin = confirm("Are you administrator?");
+//
+// alert(isAdmin);
+
+var name = prompt('what is your name?', 'name');
+alert('Your name is ' + name)

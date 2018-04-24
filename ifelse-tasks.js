b9090eb658cc6476f4b6853f69f28d8ddc9aab8b
y-tikhenkiy <y.tikhenkiy@gmail.com>
2018-04-24 17:29:26 +0300

    add file ifelse.js doing ifelse tasks

---
+++ ifelse-tasks.js
@@ -0,0 +1,29 @@
+//task 1
+// var name = prompt("Каково 'официальное' название Javascript", '');
+//
+// if (name == 'EcmaScript'){
+//     alert('Верно!');
+// } else{
+//     alert('Не знаете? "EcmaScript"!');
+// }
+
+//task 2
+var login = prompt("Кто пришёл? ", '');
+if (login == 'Админ'){
+    var pass = prompt("Пароль? ", '');
+    if (pass == 'Черный Властелин'){
+        alert('Добро пожаловать!');
+    }
+    else if(pass == null){
+        alert('Вход отменен');
+    }
+    else{
+        alert('Пароль неверен');
+    }
+}
+else if(login == null){
+    alert('Вход отменен');
+}
+else {
+    alert('Я вас не знаю');
+}

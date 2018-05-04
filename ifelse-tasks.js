53986dbee6ea9fa156e8c4d736b9ccc76c980742
y-tikhenkiy <y.tikhenkiy@gmail.com>
2018-05-04 16:27:22 +0300

    add some code to bitwise_operators.js & first.html

--- ifelse-tasks.js
+++ ifelse-tasks.js
@@ -8,22 +8,28 @@
 // }
 
 //task 2
-var login = prompt("Кто пришёл? ", '');
-if (login == 'Админ'){
-    var pass = prompt("Пароль? ", '');
-    if (pass == 'Черный Властелин'){
-        alert('Добро пожаловать!');
-    }
-    else if(pass == null){
-        alert('Вход отменен');
-    }
-    else{
-        alert('Пароль неверен');
-    }
-}
-else if(login == null){
-    alert('Вход отменен');
-}
-else {
-    alert('Я вас не знаю');
-}
+// var login = prompt("Кто пришёл? ", '');
+// if (login == 'Админ'){
+//     var pass = prompt("Пароль? ", '');
+//     if (pass == 'Черный Властелин'){
+//         alert('Добро пожаловать!');
+//     }
+//     else if(pass == null){
+//         alert('Вход отменен');
+//     }
+//     else{
+//         alert('Пароль неверен');
+//     }
+// }
+// else if(login == null){
+//     alert('Вход отменен');
+// }
+// else {
+//     alert('Я вас не знаю');
+// }
+
+// task 3
+var message = (login == 'Вася') ? 'Hello' :
+    (login == 'Директор') ? 'Здравствуйте!':
+        (login == '') ? 'Нет логина':
+            '';
